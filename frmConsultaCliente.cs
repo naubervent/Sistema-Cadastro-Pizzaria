35adc2c621236e17754ee359debbc2079af0a37d
Nauber Ventura <naubervent@gmail.com>
2017-02-21 16:43:32 -0300

    Cadastro de Clientes - Windows Form
    Cadastro de clientes

---
+++ frmConsultaCliente.cs
@@ -0,0 +1,44 @@
+﻿using PizzariaSys.Dominio;
+using PizzariaSys.Dominio.Negocios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzariaSys.WindowsForm
+{
+    public partial class frmConsultaCliente : Form
+    {
+        public frmConsultaCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            ClienteNegocios clienteNegocios = new ClienteNegocios();
+            Cliente cliente = new Cliente();
+
+            cliente = clienteNegocios.ListarClienteTelefone(txtConsulta.Text);
+
+            if (cliente == null)
+            {
+                MessageBox.Show("Cliente não localizado", "AVIZO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            frmCadastro frm = new frmCadastro(AcaoNaTela.Consultar, cliente);
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ frmPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzariaSys.WindowsForm
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCadastro frm = new frmCadastro(AcaoNaTela.Inserir, null);
+            frm.ShowDialog();
+        }
+
+        private void consultaClienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmConsultaCliente frm = new frmConsultaCliente();
+            frm.ShowDialog();
+        }
+    }
+}
